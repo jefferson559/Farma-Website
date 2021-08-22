6351a6f772a7d61427036d506eacb6b90ec5d2c6
Jefferson <jeffersonperezinfo@gmail.com>
2021-08-22 17:21:01 -0300

    add rquest url

--- Utils/data.js
+++ Utils/data.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const getBanners = (callback) => {
-    request('http://localhost:3001/banners', (error, res, body) => {
+    request('https://api-farma.herokuapp.com/banners', (error, res, body) => {
         if(error){
           console.log('Error', error)  
           return callback(error, undefined);
@@ -15,7 +15,7 @@ const getBanners = (callback) => {
     });
 }
 const getAllProducts = (callback) => {
-    request('http://localhost:3001/products', (error, res, body) => {
+    request('https://api-farma.herokuapp.com//products', (error, res, body) => {
         if(error){
           console.log('Error', error)  
           return callback(error, undefined);
